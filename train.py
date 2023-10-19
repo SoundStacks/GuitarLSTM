f7165c798be8ce78ccc5a84d7dbb272cd1231dfa
Cesare Ferrari <cesare.ferrari@gmail.com>
2023-10-19 22:10:46 +0100

    Added RTNeural/Cmajor scripting

--- train.py
+++ train.py
@@ -7,6 +7,7 @@ from tensorflow.keras.activations import tanh, elu, relu
 from tensorflow.keras.models import load_model
 import tensorflow.keras.backend as K
 from tensorflow.keras.utils import Sequence
+from model_utils import save_model
 
 import os
 from scipy import signal
@@ -17,17 +18,17 @@ import math
 import h5py
 import argparse
 
-   
+
 def pre_emphasis_filter(x, coeff=0.95):
     return tf.concat([x, x - coeff * x], 1)
-    
-def error_to_signal(y_true, y_pred): 
+
+def error_to_signal(y_true, y_pred):
     """
     Error to signal ratio with pre-emphasis filter:
     """
     y_true, y_pred = pre_emphasis_filter(y_true), pre_emphasis_filter(y_pred)
     return K.sum(tf.pow(y_true - y_pred, 2), axis=0) / K.sum(tf.pow(y_true, 2), axis=0) + 1e-10
-    
+
 def save_wav(name, data):
     wavfile.write(name, 44100, data.flatten().astype(np.float32))
 
@@ -42,9 +43,9 @@ def main(args):
         "Real-Time Guitar Amplifier Emulation with Deep Learning"
         https://www.mdpi.com/2076-3417/10/3/766/htm
 
-        Uses a stack of two 1-D Convolutional layers, followed by LSTM, followed by 
-        a Dense (fully connected) layer. Three preset training modes are available, 
-        with further customization by editing the code. A Sequential tf.keras model 
+        Uses a stack of two 1-D Convolutional layers, followed by LSTM, followed by
+        a Dense (fully connected) layer. Three preset training modes are available,
+        with further customization by editing the code. A Sequential tf.keras model
         is implemented here.
 
         Note: RAM may be a limiting factor for the parameter "input_size". The wav data
@@ -53,7 +54,7 @@ def main(args):
           RAM issues. Also, you can use the "--split_data" option to divide the data by the
           specified amount and train the model on each set. Doing this will allow for a higher
           input_size setting (more accurate results).
-        
+
         --training_mode=0   Speed training (default)
         --training_mode=1   Accuracy training
         --training_mode=2   Extended training (set max_epochs as desired, for example 50+)
@@ -66,27 +67,27 @@ def main(args):
         print("A model folder with the same name already exists. Please choose a new name.")
         return
 
-    train_mode = args.training_mode     # 0 = speed training, 
-                                        # 1 = accuracy training 
+    train_mode = args.training_mode     # 0 = speed training,
+                                        # 1 = accuracy training
                                         # 2 = extended training
-    batch_size = args.batch_size 
+    batch_size = args.batch_size
     test_size = 0.2
     epochs = args.max_epochs
     input_size = args.input_size
 
     # TRAINING MODE
     if train_mode == 0:         # Speed Training
-        learning_rate = 0.01 
-        conv1d_strides = 12    
+        learning_rate = 0.01
+        conv1d_strides = 12
         conv1d_filters = 16
         hidden_units = 36
     elif train_mode == 1:       # Accuracy Training (~10x longer than Speed Training)
-        learning_rate = 0.01 
+        learning_rate = 0.01
         conv1d_strides = 4
         conv1d_filters = 36
         hidden_units= 64
     else:                       # Extended Training (~60x longer than Accuracy Training)
-        learning_rate = 0.0005 
+        learning_rate = 0.0005
         conv1d_strides = 3
         conv1d_filters = 36
         hidden_units= 96
@@ -105,10 +106,10 @@ def main(args):
     in_rate, in_data = wavfile.read(args.in_file)
     out_rate, out_data = wavfile.read(args.out_file)
 
-    X_all = in_data.astype(np.float32).flatten()  
-    X_all = normalize(X_all).reshape(len(X_all),1)   
-    y_all = out_data.astype(np.float32).flatten() 
-    y_all = normalize(y_all).reshape(len(y_all),1)   
+    X_all = in_data.astype(np.float32).flatten()
+    X_all = normalize(X_all).reshape(len(X_all),1)
+    y_all = out_data.astype(np.float32).flatten()
+    y_all = normalize(y_all).reshape(len(y_all),1)
 
     # If splitting the data for training, do this part
     if args.split_data > 1:
@@ -124,34 +125,34 @@ def main(args):
             X_split = X_data[i]
             y_split = y_data[i]
 
-            y_ordered = y_split[input_size-1:] 
+            y_ordered = y_split[input_size-1:]
 
-            indices = np.arange(input_size) + np.arange(len(X_split)-input_size+1)[:,np.newaxis] 
-            X_ordered = tf.gather(X_split,indices) 
+            indices = np.arange(input_size) + np.arange(len(X_split)-input_size+1)[:,np.newaxis]
+            X_ordered = tf.gather(X_split,indices)
 
-            shuffled_indices = np.random.permutation(len(X_ordered)) 
+            shuffled_indices = np.random.permutation(len(X_ordered))
             X_random = tf.gather(X_ordered,shuffled_indices)
             y_random = tf.gather(y_ordered, shuffled_indices)
 
             # Train Model ###################################################
-            model.fit(X_random,y_random, epochs=epochs, batch_size=batch_size, validation_split=0.2)  
- 
+            model.fit(X_random,y_random, epochs=epochs, batch_size=batch_size, validation_split=0.2)
+
 
         model.save('models/'+name+'/'+name+'.h5')
 
     # If training on the full set of input data in one run, do this part
     else:
-        y_ordered = y_all[input_size-1:] 
+        y_ordered = y_all[input_size-1:]
 
-        indices = np.arange(input_size) + np.arange(len(X_all)-input_size+1)[:,np.newaxis] 
-        X_ordered = tf.gather(X_all,indices) 
+        indices = np.arange(input_size) + np.arange(len(X_all)-input_size+1)[:,np.newaxis]
+        X_ordered = tf.gather(X_all,indices)
 
-        shuffled_indices = np.random.permutation(len(X_ordered)) 
+        shuffled_indices = np.random.permutation(len(X_ordered))
         X_random = tf.gather(X_ordered,shuffled_indices)
         y_random = tf.gather(y_ordered, shuffled_indices)
 
         # Train Model ###################################################
-        model.fit(X_random,y_random, epochs=epochs, batch_size=batch_size, validation_split=test_size)    
+        model.fit(X_random,y_random, epochs=epochs, batch_size=batch_size, validation_split=test_size)
 
         model.save('models/'+name+'/'+name+'.h5')
 
@@ -161,9 +162,9 @@ def main(args):
     # Get the last 20% of the wav data to run prediction and plot results
     y_the_rest, y_last_part = np.split(y_all, [int(len(y_all)*.8)])
     x_the_rest, x_last_part = np.split(X_all, [int(len(X_all)*.8)])
-    y_test = y_last_part[input_size-1:] 
-    indices = np.arange(input_size) + np.arange(len(x_last_part)-input_size+1)[:,np.newaxis] 
-    X_test = tf.gather(x_last_part,indices) 
+    y_test = y_last_part[input_size-1:]
+    indices = np.arange(input_size) + np.arange(len(x_last_part)-input_size+1)[:,np.newaxis]
+    X_test = tf.gather(x_last_part,indices)
 
     prediction = model.predict(X_test, batch_size=batch_size)
 
@@ -178,6 +179,8 @@ def main(args):
     dset = grp.create_dataset("input_size", (1,), dtype='int16')
     dset[0] = input_size
     f.close()
+    save_model (model, f"models/{name}/rt.json")
+    os.system (f"rtneural/rtneuralToCmajor.py --model models/{name}/rt.json --output models/{name}/patch")
 
     # Create Analysis Plots ###########################################
     if args.create_plots == 1:
@@ -185,7 +188,7 @@ def main(args):
         import plot
 
         plot.analyze_pred_vs_actual({   'output_wav':'models/'+name+'/y_test.wav',
-                                            'pred_wav':'models/'+name+'/y_pred.wav', 
+                                            'pred_wav':'models/'+name+'/y_pred.wav',
                                             'input_wav':'models/'+name+'/x_test.wav',
                                             'model_name':name,
                                             'show_plots':1,
